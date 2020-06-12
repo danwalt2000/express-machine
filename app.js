cc3ce1b8b5ed738497636c608157b3e1209fc6fa
danwalt2000 <danwalt@mail.ru>
2020-06-13 00:09:42 +0300

    Refactoring for more modularity

--- app.js
+++ app.js
@@ -1,108 +1,22 @@
 const express = require('express');
 const app = express();
 
-// Serves Express Yourself website
-app.use(express.static('public'));
-
 const { getElementById, getIndexById, updateElement,
-        seedElements, createElement } = require('./utils');
-
-const expressions = [];
-seedElements(expressions, 'expressions');
-const animals = [];
-seedElements(animals, 'animals');
+  seedElements, createElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 // Use static server to serve the Express Yourself Website
 app.use(express.static('public'));
 
-app.get('/expressions', (req, res, next) => {
-  res.send(expressions);
-});
 
-app.get('/expressions/:id', (req, res, next) => {
-  const foundExpression = getElementById(req.params.id, expressions);
-  if (foundExpression) {
-    res.send(foundExpression);
-  } else {
-    res.status(404).send();
-  }
-});
 
-app.put('/expressions/:id', (req, res, next) => {
-  const expressionIndex = getIndexById(req.params.id, expressions);
-  if (expressionIndex !== -1) {
-    updateElement(req.params.id, req.query, expressions);
-    res.send(expressions[expressionIndex]);
-  } else {
-    res.status(404).send();
-  }
-});
+// Import and mount the expressionsRouter
+const expressionsRouter = require('./expressions.js');
+app.use('/expressions', expressionsRouter);
 
-app.post('/expressions', (req, res, next) => {
-  const receivedExpression = createElement('expressions', req.query);
-  if (receivedExpression) {
-    expressions.push(receivedExpression);
-    res.status(201).send(receivedExpression);
-  } else {
-    res.status(400).send();
-  }
-});
-
-app.delete('/expressions/:id', (req, res, next) => {
-  const expressionIndex = getIndexById(req.params.id, expressions);
-  if (expressionIndex !== -1) {
-    expressions.splice(expressionIndex, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
-});
-
-app.get("/animals", (req, res, next)=>{
-  res.send(animals);
-});
-
-app.get("/animals/:id", (req, res, next)=>{
-  const indexOfId = getIndexById(req.params.id, animals);
-  if(indexOfId !== -1){
-    const foundAnimal = getElementById(req.params.id, animals);
-    res.send(foundAnimal);
-  } else{
-    req.status(404).send();
-  }  
-});
-
-app.put("/animals/:id", (req, res, next)=>{
-  const indexOfId = getIndexById(req.params.id, animals);
-  if(indexOfId !== -1){
-    updateElement(req.params.id, req.query, animals);
-    res.send(animals[indexOfId]);
-  } else{
-    res.status(404).send();
-  }  
-});
-
-app.post("/animals", (req, res, next)=>{
-  const receivedAnimal = createElement('animals', req.query);
-  if (receivedAnimal) {
-    animals.push(receivedAnimal);
-    res.status(201).send(receivedAnimal);
-  } else {
-    res.status(400).send();
-  }
-});
-
-app.delete('/animals/:id', (req, res, next) => {
-  const animalIndex = getIndexById(req.params.id, animals);
-  if (animalIndex !== -1) {
-    animals.splice(animalIndex, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
-});
+const animalsRouter = require('./animals.js');
+app.use('/animals', animalsRouter);
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`); 
+  console.log(`Server is listening on ${PORT}`);
 });
